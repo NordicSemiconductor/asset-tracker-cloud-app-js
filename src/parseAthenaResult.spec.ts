4dd611685eee6e7154fdc3497807f92fc46831fe
Markus Tacker <m@coderbyheart.com>
2019-08-05 20:58:57 +0200

    test: fix parseAthenaResult test

--- src/parseAthenaResult.spec.ts
+++ src/parseAthenaResult.spec.ts
@@ -1,51 +1,53 @@
 import { parseAthenaResult } from './parseAthenaResult'
 
 describe('parseAthenaResult', () => {
-	it('parse an Athena result into an array of values', () => {
+	it('parses an Athena result into an array of values', () => {
 		expect(
 			parseAthenaResult({
-				Rows: [
-					{ Data: [{ VarCharValue: 'date' }, { VarCharValue: 'value' }] },
-					{
-						Data: [
-							{ VarCharValue: '2019-08-01T10:29:54.406Z' },
-							{ VarCharValue: '2607' },
-						],
-					},
-					{
-						Data: [
-							{ VarCharValue: '2019-07-31T08:34:20.765Z' },
-							{ VarCharValue: '2046' },
-						],
-					},
-				],
-				ResultSetMetadata: {
-					ColumnInfo: [
+				ResultSet: {
+					Rows: [
+						{ Data: [{ VarCharValue: 'date' }, { VarCharValue: 'value' }] },
 						{
-							CatalogName: 'hive',
-							SchemaName: '',
-							TableName: '',
-							Name: 'date',
-							Label: 'date',
-							Type: 'varchar',
-							Precision: 2147483647,
-							Scale: 0,
-							Nullable: 'UNKNOWN',
-							CaseSensitive: true,
+							Data: [
+								{ VarCharValue: '2019-08-01T10:29:54.406Z' },
+								{ VarCharValue: '2607' },
+							],
 						},
 						{
-							CatalogName: 'hive',
-							SchemaName: '',
-							TableName: '',
-							Name: 'value',
-							Label: 'value',
-							Type: 'integer',
-							Precision: 10,
-							Scale: 0,
-							Nullable: 'UNKNOWN',
-							CaseSensitive: false,
+							Data: [
+								{ VarCharValue: '2019-07-31T08:34:20.765Z' },
+								{ VarCharValue: '2046' },
+							],
 						},
 					],
+					ResultSetMetadata: {
+						ColumnInfo: [
+							{
+								CatalogName: 'hive',
+								SchemaName: '',
+								TableName: '',
+								Name: 'date',
+								Label: 'date',
+								Type: 'varchar',
+								Precision: 2147483647,
+								Scale: 0,
+								Nullable: 'UNKNOWN',
+								CaseSensitive: true,
+							},
+							{
+								CatalogName: 'hive',
+								SchemaName: '',
+								TableName: '',
+								Name: 'value',
+								Label: 'value',
+								Type: 'integer',
+								Precision: 10,
+								Scale: 0,
+								Nullable: 'UNKNOWN',
+								CaseSensitive: false,
+							},
+						],
+					},
 				},
 			}),
 		).toEqual([
