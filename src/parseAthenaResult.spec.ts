ca1d38237f1e6f4701247c71b693b5cfc74457ba
Markus Tacker <m@coderbyheart.com>
2019-08-05 21:00:51 +0200

    test: add formatter test for parseAthenaResult

--- src/parseAthenaResult.spec.ts
+++ src/parseAthenaResult.spec.ts
@@ -1,54 +1,56 @@
 import { parseAthenaResult } from './parseAthenaResult'
 
+const ResultSet = {
+	Rows: [
+		{ Data: [{ VarCharValue: 'date' }, { VarCharValue: 'value' }] },
+		{
+			Data: [
+				{ VarCharValue: '2019-08-01T10:29:54.406Z' },
+				{ VarCharValue: '2607' },
+			],
+		},
+		{
+			Data: [
+				{ VarCharValue: '2019-07-31T08:34:20.765Z' },
+				{ VarCharValue: '2046' },
+			],
+		},
+	],
+	ResultSetMetadata: {
+		ColumnInfo: [
+			{
+				CatalogName: 'hive',
+				SchemaName: '',
+				TableName: '',
+				Name: 'date',
+				Label: 'date',
+				Type: 'varchar',
+				Precision: 2147483647,
+				Scale: 0,
+				Nullable: 'UNKNOWN',
+				CaseSensitive: true,
+			},
+			{
+				CatalogName: 'hive',
+				SchemaName: '',
+				TableName: '',
+				Name: 'value',
+				Label: 'value',
+				Type: 'integer',
+				Precision: 10,
+				Scale: 0,
+				Nullable: 'UNKNOWN',
+				CaseSensitive: false,
+			},
+		],
+	},
+}
+
 describe('parseAthenaResult', () => {
 	it('parses an Athena result into an array of values', () => {
 		expect(
 			parseAthenaResult({
-				ResultSet: {
-					Rows: [
-						{ Data: [{ VarCharValue: 'date' }, { VarCharValue: 'value' }] },
-						{
-							Data: [
-								{ VarCharValue: '2019-08-01T10:29:54.406Z' },
-								{ VarCharValue: '2607' },
-							],
-						},
-						{
-							Data: [
-								{ VarCharValue: '2019-07-31T08:34:20.765Z' },
-								{ VarCharValue: '2046' },
-							],
-						},
-					],
-					ResultSetMetadata: {
-						ColumnInfo: [
-							{
-								CatalogName: 'hive',
-								SchemaName: '',
-								TableName: '',
-								Name: 'date',
-								Label: 'date',
-								Type: 'varchar',
-								Precision: 2147483647,
-								Scale: 0,
-								Nullable: 'UNKNOWN',
-								CaseSensitive: true,
-							},
-							{
-								CatalogName: 'hive',
-								SchemaName: '',
-								TableName: '',
-								Name: 'value',
-								Label: 'value',
-								Type: 'integer',
-								Precision: 10,
-								Scale: 0,
-								Nullable: 'UNKNOWN',
-								CaseSensitive: false,
-							},
-						],
-					},
-				},
+				ResultSet,
 			}),
 		).toEqual([
 			{
@@ -58,6 +60,26 @@ describe('parseAthenaResult', () => {
 			{
 				date: '2019-07-31T08:34:20.765Z',
 				value: 2046,
+			},
+		])
+	})
+
+	it('can accept formatters to customize row formatting', () => {
+		expect(
+			parseAthenaResult({
+				ResultSet,
+				formatters: {
+					integer: v => parseInt(v, 10) / 1000,
+				},
+			}),
+		).toEqual([
+			{
+				date: '2019-08-01T10:29:54.406Z',
+				value: 2.607,
+			},
+			{
+				date: '2019-07-31T08:34:20.765Z',
+				value: 2.046,
 			},
 		])
 	})
