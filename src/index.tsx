06d390c51317594bb04822ef22658cd19f271694
Markus Tacker <Markus.Tacker@NordicSemi.no>
2019-12-05 17:41:30 +0100

    feat: split code to reduce bundle size

--- src/index.tsx
+++ src/index.tsx
@@ -1,35 +1,54 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import { boot as bootAWS } from './aws/App'
-import { boot as bootGCP } from './gcp/App'
-import { boot as bootAzure } from './azure/App'
 
 const cloudFlavour = process.env.REACT_APP_CLOUD_FLAVOUR
 
-let App
+const launch = (App: any) => {
+	ReactDOM.render(<App />, document.getElementById('root'))
+}
+const onError = (err: Error) => {
+	console.error(err)
+}
 switch (cloudFlavour) {
 	case 'GCP':
 		console.log(`Launching Google Cloud Platform app ...`)
-		App = bootGCP({
-			apiKey: process.env.REACT_APP_FIREBASE_API_KEY || '',
-			authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || '',
-		})
+		import('./gcp/App')
+			.then(gcpApp => {
+				launch(
+					gcpApp.boot({
+						apiKey: process.env.REACT_APP_FIREBASE_API_KEY || '',
+						authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || '',
+					}),
+				)
+			})
+			.catch(onError)
 		break
 	case 'AZURE':
 		console.log(`Launching Microsoft Azure app ...`)
-		App = bootAzure({
-			clientId: process.env.REACT_APP_AZURE_CLIENT_ID || '',
-			redirectUri: process.env.REACT_APP_AZURE_REDIRECT_URI || '',
-		})
+		import('./azure/App')
+			.then(azureApp => {
+				launch(
+					azureApp.boot({
+						clientId: process.env.REACT_APP_AZURE_CLIENT_ID || '',
+						redirectUri: process.env.REACT_APP_AZURE_REDIRECT_URI || '',
+					}),
+				)
+			})
+			.catch(onError)
 		break
 	default:
 		console.log(`Launching Amazon Webservices app ...`)
-		App = bootAWS({
-			identityPoolId: process.env.REACT_APP_IDENTITY_POOL_ID || '',
-			region: process.env.REACT_APP_REGION || '',
-			userPoolId: process.env.REACT_APP_USER_POOL_ID || '',
-			userPoolWebClientId: process.env.REACT_APP_USER_POOL_CLIENT_ID || '',
-		})
+		import('./aws/App')
+			.then(awsApp => {
+				launch(
+					awsApp.boot({
+						identityPoolId: process.env.REACT_APP_IDENTITY_POOL_ID || '',
+						region: process.env.REACT_APP_REGION || '',
+						userPoolId: process.env.REACT_APP_USER_POOL_ID || '',
+						userPoolWebClientId:
+							process.env.REACT_APP_USER_POOL_CLIENT_ID || '',
+					}),
+				)
+			})
+			.catch(onError)
 }
-
-ReactDOM.render(<App />, document.getElementById('root'))
