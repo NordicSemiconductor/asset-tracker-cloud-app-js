d61f08cca85259a0a7266222eff08dadb1edafc0
Markus Tacker <Markus.Tacker@NordicSemi.no>
2020-02-19 11:47:09 +0100

    refactor: extracted RSRP bar

--- src/Cat/ConnectionInformation.tsx
+++ src/Cat/ConnectionInformation.tsx
@@ -4,50 +4,33 @@ import { ReportedTime } from './ReportedTime'
 import { DeviceInformation, RoamingInformation } from '../@types/DeviceShadow'
 import { TextWithIcon } from '../TextWithIcon/TextWithIcon'
 import { emojify } from '../Emojify/Emojify'
-import { rsrpToPercent } from './rsrpToPercent'
 import styled from 'styled-components'
-import { RSRPBar } from './RSRPBar'
+import { RSRP, RSRPBar } from '@bifravst/rsrp-bar'
 
 const StyledRSRPBar = styled(RSRPBar)`
 	width: 20px;
 	height: 20px;
 `
-/**
- * Renders the Reference Signal Received Power (RSRP).
- *
- * RSRP is the average power level received from a single reference signal in an LTE (Long-term Evolution) network.
- *
- * 0: RSRP < −140 dBm
- * 1: – When −140 dBm ≤ RSRP < −139 dBm
- * 2: When −139 dBm ≤ RSRP < −138 dBm
- * ..95: When −46 dBm ≤ RSRP < −45 dBm
- * 96: When −45 dBm ≤ RSRP < −44 dBm
- * 97: When −44 dBm ≤ RSRP
- * 255: Not known or not detectable
- */
-export const RSRP = ({ rsrp: { value } }: { rsrp: { value: number } }) => {
-	if (value === 255) {
-		return (
-			<abbr title={'Not known or not detectable'}>
-				<StyledRSRPBar quality={0} />
-			</abbr>
-		)
-	}
-
-	if (value >= 0 && value <= 140) {
-		const dbm = -140 + value
-		const quality = rsrpToPercent(dbm)
-		return (
-			<>
-				<StyledRSRPBar quality={quality} />
-				<small>{`(${dbm}dBm)`}</small>
-			</>
-		)
-	}
-	return (
-		<abbr title={`Unexpected value ${value} reported!`}>{emojify('❎')}</abbr>
-	)
-}
+const signalQuality = (rsrp: number) => (
+	<RSRP
+		rsrp={rsrp}
+		renderBar={({ quality, dbm }) =>
+			quality === 0 ? (
+				<abbr title={'Not known or not detectable'}>
+					<StyledRSRPBar quality={0} />
+				</abbr>
+			) : (
+				<>
+					<StyledRSRPBar quality={quality} />
+					<small>{`(${dbm}dBm)`}</small>
+				</>
+			)
+		}
+		renderInvalid={() => (
+			<abbr title={`Unexpected value ${rsrp} reported!`}>{emojify('❎')}</abbr>
+		)}
+	/>
+)
 
 export const Operator = ({ op }: { op?: Op }) => (
 	<span className={'operator'}>
@@ -72,7 +55,7 @@ export const ConnectionInformation = ({
 	const nw = device && device.v.nw.value
 	return (
 		<div className={'info connection-information'}>
-			<TextWithIcon icon={<RSRP rsrp={rsrp} />}>
+			<TextWithIcon icon={signalQuality(rsrp.value)}>
 				<>
 					&nbsp;
 					<Operator op={filterOperator({ mccmnc: `${mccmnc}` })[0]} />
--- src/Cat/rsrpToPercent.ts
+++
@@ -1,7 +0,0 @@
-export const rsrpToPercent = (rsrp: number): number => {
-	if (rsrp <= -110) return 0
-	if (rsrp < -70) {
-		return (rsrp + 110) / 40
-	}
-	return 1
-}
