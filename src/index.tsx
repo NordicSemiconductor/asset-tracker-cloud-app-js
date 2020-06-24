e4f6f762a08bd916e65e7295d9d156d46c95d4de
Markus Tacker <Markus.Tacker@NordicSemi.no>
2020-06-24 14:24:29 +0200

    fix: log version

--- src/index.tsx
+++ src/index.tsx
@@ -21,12 +21,15 @@ const ReactAppConfig = React.createContext<ReactAppConfig>({
 })
 export const ReactAppConfigConsumer = ReactAppConfig.Consumer
 
+const version = process.env.REACT_APP_VERSION ?? '0.0.0-development'
+
 const launch = (App: any) => {
+	console.log(`Launching ${cloudFlavour} app ${version}...`)
 	ReactDOM.render(
 		<ReactAppConfig.Provider
 			value={{
 				cloudFlavour,
-				version: process.env.REACT_APP_VERSION || '0.0.0-development',
+				version,
 			}}
 		>
 			<App />
@@ -41,11 +44,11 @@ switch (cloudFlavour) {
 	case CloudFlavour.GCP:
 		console.log(`Launching Google Cloud Platform app ...`)
 		import('./gcp/App')
-			.then(gcpApp => {
+			.then((gcpApp) => {
 				launch(
 					gcpApp.boot({
-						apiKey: process.env.REACT_APP_FIREBASE_API_KEY || '',
-						authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || '',
+						apiKey: process.env.REACT_APP_FIREBASE_API_KEY ?? '',
+						authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN ?? '',
 					}),
 				)
 			})
@@ -54,15 +57,15 @@ switch (cloudFlavour) {
 	case CloudFlavour.Azure:
 		console.log(`Launching Microsoft Azure app ...`)
 		import('./azure/App')
-			.then(azureApp => {
+			.then((azureApp) => {
 				launch(
 					azureApp.boot({
 						apiEndpoint: (
-							process.env.REACT_APP_AZURE_API_ENDPOINT || ''
+							process.env.REACT_APP_AZURE_API_ENDPOINT ?? ''
 						).replace(/\/+$/, ''),
-						clientId: process.env.REACT_APP_AZURE_CLIENT_ID || '',
-						redirectUri: process.env.REACT_APP_AZURE_REDIRECT_URI || '',
-						authority: process.env.REACT_APP_AZURE_AD_B2C_AUTHORITY || '',
+						clientId: process.env.REACT_APP_AZURE_CLIENT_ID ?? '',
+						redirectUri: process.env.REACT_APP_AZURE_REDIRECT_URI ?? '',
+						authority: process.env.REACT_APP_AZURE_AD_B2C_AUTHORITY ?? '',
 					}),
 				)
 			})
@@ -72,29 +75,29 @@ switch (cloudFlavour) {
 	default:
 		console.log(`Launching Amazon Webservices app ...`)
 		import('./aws/App')
-			.then(awsApp => {
+			.then((awsApp) => {
 				launch(
 					awsApp.boot({
-						identityPoolId: process.env.REACT_APP_IDENTITY_POOL_ID || '',
-						region: process.env.REACT_APP_REGION || '',
-						userPoolId: process.env.REACT_APP_USER_POOL_ID || '',
-						userPoolClientId: process.env.REACT_APP_USER_POOL_CLIENT_ID || '',
+						identityPoolId: process.env.REACT_APP_IDENTITY_POOL_ID ?? '',
+						region: process.env.REACT_APP_REGION ?? '',
+						userPoolId: process.env.REACT_APP_USER_POOL_ID ?? '',
+						userPoolClientId: process.env.REACT_APP_USER_POOL_CLIENT_ID ?? '',
 						athenaConfig: {
 							workGroup:
-								process.env.REACT_APP_HISTORICALDATA_WORKGROUP_NAME || '',
+								process.env.REACT_APP_HISTORICALDATA_WORKGROUP_NAME ?? '',
 							dataBase:
-								process.env.REACT_APP_HISTORICALDATA_DATABASE_NAME || '',
+								process.env.REACT_APP_HISTORICALDATA_DATABASE_NAME ?? '',
 							rawDataTable:
-								process.env.REACT_APP_HISTORICALDATA_TABLE_NAME || '',
+								process.env.REACT_APP_HISTORICALDATA_TABLE_NAME ?? '',
 							bucketName:
-								process.env.REACT_APP_HISTORICAL_DATA_BUCKET_NAME || '',
+								process.env.REACT_APP_HISTORICAL_DATA_BUCKET_NAME ?? '',
 						},
-						mqttEndpoint: process.env.REACT_APP_MQTT_ENDPOINT || '',
-						userIotPolicyArn: process.env.REACT_APP_USER_IOT_POLICY_ARN || '',
-						avatarBucketName: process.env.REACT_APP_AVATAR_BUCKET_NAME || '',
-						fotaBucketName: process.env.REACT_APP_FOTA_BUCKET_NAME || '',
+						mqttEndpoint: process.env.REACT_APP_MQTT_ENDPOINT ?? '',
+						userIotPolicyArn: process.env.REACT_APP_USER_IOT_POLICY_ARN ?? '',
+						avatarBucketName: process.env.REACT_APP_AVATAR_BUCKET_NAME ?? '',
+						fotaBucketName: process.env.REACT_APP_FOTA_BUCKET_NAME ?? '',
 						geolocationApiEndpoint: (
-							process.env.REACT_APP_GEOLOCATION_API_URL || ''
+							process.env.REACT_APP_GEOLOCATION_API_URL ?? ''
 						).replace(/\/+$/, ''),
 					}),
 				)
