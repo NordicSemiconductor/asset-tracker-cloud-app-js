e54706c9328dee43e027d931c00ab5b841cae037
Lena Haraldseid <lena.haraldseid@nordicsemi.no>
2023-11-08 10:59:16 +0100

    test: replaces jest with Node.js test runner

--- src/util/nullOrUndefined.spec.ts
+++ src/util/nullOrUndefined.spec.ts
@@ -1,13 +1,15 @@
+import assert from 'node:assert'
+import { describe, test as it } from 'node:test'
 import { nullOrUndefined } from './nullOrUndefined'
 
 describe('nullOrUndefined', () => {
-	it('should return true for undefined', () => {
-		expect(nullOrUndefined()).toEqual(true)
+	void it('should return true for undefined', () => {
+		assert.equal(nullOrUndefined(), true)
 	})
-	it('should return true for null', () => {
-		expect(nullOrUndefined(null)).toEqual(true)
+	void it('should return true for null', () => {
+		assert.equal(nullOrUndefined(null), true)
 	})
-	it('should return false for a value', () => {
-		expect(nullOrUndefined('some')).toEqual(false)
+	void it('should return false for a value', () => {
+		assert.equal(nullOrUndefined('some'), false)
 	})
 })
