eb569aa7919c52de7ca604b98f54e36866f4ea98
snmb22 <160770537+snmb22@users.noreply.github.com>
2024-05-20 06:31:23 -0400

    Delete src directory

--- src/@types/Message.d.ts
+++
@@ -1,9 +0,0 @@
-export type Button = {
-	v: number
-	ts: Date
-}
-
-export type Message = {
-	btn?: Button
-	impact?: Button
-}
--- src/User/validation/StringEquals.ts
+++
@@ -1,6 +0,0 @@
-import { Either, left, right } from 'fp-ts/lib/Either'
-
-export const StringEquals =
-	(s1: string) =>
-	(s2: string): Either<Error, string> =>
-		s1 === s2 ? right(s1) : left(new Error('Strings do not match!'))
